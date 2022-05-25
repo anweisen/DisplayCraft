432af4dccdcaf380922b5af098466689569ac40e
anweisen <anweisen@web.de>
2022-05-25 19:16:40 +0200

    Create plugin

---
+++ src/main/java/net/anweisen/displaycraft/DisplayCraft.java
@@ -0,0 +1,14 @@
+package net.anweisen.displaycraft;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 1.0
+ */
+public class DisplayCraft extends JavaPlugin implements Listener {
+
+	@Override
+	public void onEnable() {
+	}
+
+}
