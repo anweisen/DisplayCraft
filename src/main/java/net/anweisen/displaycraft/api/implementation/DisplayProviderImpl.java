e5ff2d4daf1f4141c7fe4a8843b45175a3d91f6a
anweisen <anweisen@web.de>
2022-05-27 23:39:31 +0200

    Create api implementation

---
+++ src/main/java/net/anweisen/displaycraft/api/implementation/DisplayProviderImpl.java
@@ -0,0 +1,57 @@
+package net.anweisen.displaycraft.api.implementation;
+
+import net.anweisen.displaycraft.api.DisplayProvider;
+import net.anweisen.displaycraft.api.Position;
+import net.anweisen.displaycraft.nms.NmsProvider;
+import net.anweisen.displaycraft.nms.Reflect;
+import org.bukkit.entity.Player;
+import org.bukkit.map.MapPalette;
+import javax.annotation.Nonnull;
+import java.awt.*;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 1.0
+ */
+public class DisplayProviderImpl implements DisplayProvider {
+
+  private final NmsProvider provider;
+
+  public DisplayProviderImpl(@Nonnull NmsProvider provider) {
+    this.provider = provider;
+  }
+
+  @Override
+  public void render(@Nonnull Player player, int mapId, int offsetX, int offsetY, int width, int height, @Nonnull byte[] content) {
+    provider.sendPacket(
+      provider.getPlayerConnection(provider.getNmsPlayer(player)),
+      provider.getMapUpdatePacket(mapId, offsetX, offsetY, width, height, content)
+    );
+  }
+
+  @Override
+  @SuppressWarnings("deprecation")
+  public void render(@Nonnull Player player, int mapId, @Nonnull Image image) {
+    render(player, mapId, 0, 0, image.getWidth(null), image.getHeight(null), MapPalette.imageToBytes(image));
+  }
+
+  @Override
+  public int create(@Nonnull Player player, @Nonnull Position position, int mapId) {
+    Object nmsPlayer = provider.getNmsPlayer(player);
+    Object playerConnection = provider.getPlayerConnection(nmsPlayer);
+
+    Object nmsEntity = provider.getItemFrameEntity(provider.getPlayerWorld(nmsPlayer), position.getX(), position.getY(), position.getZ(), position.getDirection().name());
+    int entityId = provider.getEntityId(nmsEntity);
+
+    provider.setItemFrameItem(nmsEntity, provider.getNmsItem(Reflect.newMapItem(mapId == -1 ? entityId : mapId)));
+    provider.sendPacket(playerConnection, provider.getEntitySpawnPacket(nmsEntity));
+    provider.sendPacket(playerConnection, provider.getEntityMetadataPacket(entityId, provider.getDataWatcher(nmsEntity)));
+
+    return entityId;
+  }
+
+  @Override
+  public void destroy(@Nonnull Player player, int entityId) {
+    provider.sendPacket(provider.getPlayerConnection(provider.getNmsPlayer(player)), provider.getEntityDestroyPacket(entityId));
+  }
+}
