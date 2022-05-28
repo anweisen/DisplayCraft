4a7d0042c0bc9e9491430f2ef00311e75f2b1f49
anweisen <anweisen@web.de>
2022-05-29 01:06:38 +0200

    Create desktop plugin

---
+++ desktop/src/main/java/net/anweisen/displaycraft/desktop/DisplayCraftDesktop.java
@@ -0,0 +1,22 @@
+package net.anweisen.displaycraft.desktop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author anweisen | https://github.com/anweisen
+ * @since 1.0
+ */
+public class DisplayCraftDesktop extends JavaPlugin {
+
+  @Override
+  public void onLoad() {
+  }
+
+  @Override
+  public void onEnable() {
+  }
+
+  @Override
+  public void onDisable() {
+  }
+}
